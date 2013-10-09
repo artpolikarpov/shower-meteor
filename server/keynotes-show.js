f60392034cef36af1374037b1903662c2c6c9110
Artem Polikarpov <artpolikarpov@gmail.com>
2013-10-10 02:21:27 +0400

    Fix active slide with custom url

--- server/keynotes-show.js
+++ server/keynotes-show.js
@@ -9,6 +9,8 @@ Meteor.startup(function () {
 	});
 
   Meteor.publish('keynote-show-current-slide', function (_id) {
+    var keynote = Keynotes.findOne(__.keynotes.idOrUrl(_id), {fields: {_id: 1}});
+    _id = keynote && keynote._id;
 		return _id && CurrentSlides.find({userId: {$ne: this.userId}, keynoteId: _id}, {fields: {currentSlide: 1}});
 	});
 });
