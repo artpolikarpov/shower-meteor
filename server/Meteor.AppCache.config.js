46cdd2ba5fed5dfee42ca5de5a1ba71e66cb0768
Artem Polikarpov <artpolikarpov@gmail.com>
2013-10-11 03:05:43 +0400

    Fix Meteor.AppCache.config.js

--- server/Meteor.AppCache.config.js
+++ server/Meteor.AppCache.config.js
@@ -2,9 +2,10 @@ Meteor.startup(function () {
   Meteor.AppCache.config({
     onlineOnly: [
       '/shower/',
-      '.gitignore',
-      'Gruntfile.js',
-      'package.json'
+      '/.gitignore',
+      '/Gruntfile.js',
+      '/package.json',
+      '/node_modules'
     ]
   });
 });
