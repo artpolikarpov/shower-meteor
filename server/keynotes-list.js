d63caf1d7c3e7f1171651d70ba65710e130f8597
Artem Polikarpov <artpolikarpov@gmail.com>
2013-10-10 14:27:30 +0400

    Remove unsued server method `addNewKeynote`

--- server/keynotes-list.js
+++ server/keynotes-list.js
@@ -2,10 +2,4 @@ Meteor.startup(function () {
 	Meteor.publish('keynotes-list', function () {
 		return this.userId && Keynotes.find({userId: this.userId}, {fields: {slides: 0}});
 	});
-
-	Meteor.methods({
-		'addNewKeynote': function () {
-      return Keynotes.insert(__.keynotes.emptyKeynote());
-		}
-	});
 });
